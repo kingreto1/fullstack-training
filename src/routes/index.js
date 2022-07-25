6ae006cf6aac3255ac20e9023999a6fb5dea0d61
Gabriel Melo <gabrimelo070@gmail.com>
2022-07-25 09:49:14 -0300

    configura porta do heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ const { getUser } = require('../controllers/getUser');
 const { deleteUser } = require('../controllers/deleteUser');
 const { updateUser } = require('../controllers/updateUser');
 
-const PORT = process.env.API_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
